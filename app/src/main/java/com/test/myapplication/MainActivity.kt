33f7db6b4f450ebf60f05acb9105936bb9cf1ece
i5on9i <gaedduck@gmail.com>
2021-09-06 19:38:47 +0900

    add userhabit sdk

--- app/src/main/java/com/test/myapplication/MainActivity.kt
+++ app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.test.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import io.userhabit.service.Userhabit
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Userhabit.start(application)
     }
 }
