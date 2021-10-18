7f078128c7137bb9330d5803ff87a5fe6468d86e
namh777 <namh@userhabit.io>
2021-10-18 16:56:16 +0900

    git merge from release/kt

---
+++ android/app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.test.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import io.userhabit.service.Userhabit
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
